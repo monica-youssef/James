772fc0286390cb10384ab36b66f3d9deae95e7a3
Monica Youssef <may5u@virginia.edu>
2023-10-03 20:42:04 -0400

    made more progress but source still isn't fixed

--- xml_to_json.py
+++ xml_to_json.py
@@ -44,45 +44,46 @@ def parse_xml(xml_path):
             if title_and_author.__contains__("."):
                 author = title_and_author.split(".")[1]
 
-        # COLLECT TEXT AND SOURCE
+            # COLLECT TEXT AND SOURCE
+            # if there is a number at the end drop it because it is a superscript
             commentary_and_source = section[0].text.split(":")[1:]
             commentary_and_source = ":".join(commentary_and_source)
             commentary_and_source = re.split(r'[!?\.]', commentary_and_source)
-            print(commentary_and_source)
-            print(commentary_and_source[-1])
-            print(commentary_and_source[-1].isdigit())
-            # for each in reversed(commentary_and_source):
-            #     print(each)
-            #     if each.isdigit():
-            #         source.__add__(each)
-            # print(source)
+            if commentary_and_source[-1].isdigit():
+                commentary_and_source = commentary_and_source[:-1]
 
-            # print(commentary_and_source)
+            # with the superscript gone, now we need to split commentary and source
+            last_sentence = commentary_and_source[-1]
+            numbers = ""
+            while last_sentence.isdigit() or last_sentence == "James":
+                if len(last_sentence) > 1:
+                    numbers += commentary_and_source.pop()[::-1] + "."
+                else:
+                    numbers += commentary_and_source.pop() + "."
+                last_sentence = commentary_and_source[-1].strip() if len(commentary_and_source) > 0 else ""
 
-
+            # append the numbers from the end
+            source = last_sentence + numbers[::-1]
+            print(source)
+            if len(source) > 1 and not source[0].isalpha():
+                pattern = re.compile(r'^["\d ]+')
+                if pattern.match(source):
+                    source = source[1:]
+            print(source)
 
         # COLLECT AUTHOR
-
-
-
-
 
         # COLLECT AUTHOR
         # elif
 
-
-
-
-            # if text != "":
-            #     result.append({
-            #         'text': text,
-            #         'chapter': chapter,
-            #         'verse': verse,
-            #         'author': author,
-            #         'source': source
-            #     })
-
-
+        # if text != "":
+        #     result.append({
+        #         'text': text,
+        #         'chapter': chapter,
+        #         'verse': verse,
+        #         'author': author,
+        #         'source': source
+        #     })
 
         # Iterate through runs within each section
         # for run in section.iter('run'):
